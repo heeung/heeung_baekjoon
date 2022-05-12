16f1a103a8ac918f88ca9b41823f1788b7a8b650
heeung <94839275+heeung@users.noreply.github.com>
2022-05-12 15:21:10 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int fibonacci(int N)
+{
+    if (N == 0)
+        return (0);
+    if (N == 1)
+        return (1);
+    return (fibonacci(N - 1) + fibonacci(N - 2));
+}
+
+int main(void)
+{
+    int N = 0;
+    scanf("%d", &N);
+    printf("%d\n", fibonacci(N));
+}
