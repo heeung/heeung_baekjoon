b0e222a2bf1c5a9ab881caaf8bbe3a1469ecc6dc
heeung <94839275+heeung@users.noreply.github.com>
2022-05-18 18:43:09 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 7476 ms, Memory: 8808 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const void *a, const void *b)
+{
+        return (*(int *)a - *(int *)b);
+}
+
+int main(void)
+{
+        int N, i;
+        int arr[1000001] = {0};
+
+        scanf("%d", &N);
+        for (i = 0 ; i < N ; i++)
+                scanf("%d", &arr[i]);
+        qsort(arr, N, sizeof(int), cmp);
+        for (i = 0 ; i < N ; i++)
+                printf("%d\n", arr[i]);
+}
