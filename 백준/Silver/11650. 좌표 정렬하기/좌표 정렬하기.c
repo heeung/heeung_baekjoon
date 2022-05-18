6a2e10d30ecbec6fa923f6947cd48d6368e1209f
heeung <94839275+heeung@users.noreply.github.com>
2022-05-18 19:17:58 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 80 ms, Memory: 2684 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct dot
+{
+        int x;
+        int y;
+}t_dot;
+
+int cmp_x(const void *a, const void *b)
+{
+        t_dot *d1 = (t_dot *)a;
+        t_dot *d2 = (t_dot *)b;
+
+        if (d1->x > d2->x)
+                return (1);
+        else if (d1->x < d2->x)
+                return (-1);
+        else
+                return (0);
+}
+
+int cmp_y(const void *a, const void *b)
+{
+        t_dot *d1 = (t_dot *)a;
+        t_dot *d2 = (t_dot *)b;
+
+        if (d1->y > d2->y)
+                return (1);
+        else if (d1->y < d2->y)
+                return (-1);
+        else
+                return (0);
+}
+
+int main(void)
+{
+        int N, i;
+        t_dot *dots;
+
+        scanf("%d", &N);
+        dots = (t_dot *)malloc(sizeof(t_dot) * N);
+        for (i = 0 ; i < N ; i++)
+                scanf("%d %d", &dots[i].x, &dots[i].y);
+        qsort(dots, N, sizeof(t_dot), cmp_y);
+        qsort(dots, N, sizeof(t_dot), cmp_x);
+        for (i = 0 ; i < N ; i++)
+                printf("%d %d\n", dots[i].x, dots[i].y);
+        free(dots);
+        return (0);
+}
