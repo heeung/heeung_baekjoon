9e7c7c2f8e6a704191284d8b77ac2495e74e5a9a
heeung <94839275+heeung@users.noreply.github.com>
2022-06-28 18:34:58 +0900

    [Silver IV] Title: 수 찾기, Time: 72 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+        int N, M;
+        int arr1[100002], arr2[100002];
+
+        scanf("%d", &N);
+        for (int i = 0 ; i < N ; i++)
+                scanf("%d", &arr1[i]);
+        scanf("%d", &M);
+        for (int i = 0 ; i < M ; i++)
+                scanf("%d", &arr2[i]);
+        sort(arr1, arr1 + N);
+        for (int i = 0 ; i < M ; i++)
+                printf("%d\n", binary_search(arr1, arr1 + N, arr2[i]));
+}
