37678ad2b99f61d8c002d465944a2d64e502ef39
heeung <94839275+heeung@users.noreply.github.com>
2022-08-02 23:28:52 +0900

    [Bronze III] Title: FBI, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2857. FBI/FBI.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string fbi[6];
+    string ans = "";
+    
+    for (int i = 1 ; i < 6 ; i++)
+    {
+        cin >> fbi[i];
+        if (fbi[i].find("FBI") == string::npos)
+            continue;
+        else
+            ans += to_string(i);
+    }
+    if (ans.size() == 0)
+        cout << "HE GOT AWAY!" << '\n';
+    else
+    {
+        for (int i = 0 ; i < ans.size() ; i++)
+            cout << ans[i] << ' ';
+    }
+}
