46000d36be04d2931cf3ed4bcd9701e82c4cd2f8
heeung <94839275+heeung@users.noreply.github.com>
2023-03-22 20:46:42 +0900

    [Gold III] Title: 계보 복원가 호석, Time: 1212 ms, Memory: 138720 KB -BaekjoonHub

--- 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.java
+++ 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.java
@@ -1,83 +1,125 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
 public class Main {
-	static int N,M;
-	static String[] peoples;
-	static int[] depth;
-	static HashMap<String, Integer> nameMap = new HashMap<>();
-	static HashMap<Integer, List<Integer>> map = new HashMap<>();
-	static List<String> result = new ArrayList<>();
-	static StringBuffer sb = new StringBuffer();
-	static Queue<Integer> primes = new LinkedList<>();
-	public static void main(String[] args) throws Exception{
-		init();
-		getPrimes();
-		search();
-		Collections.sort(result);
-		for(String s:result) {
-			sb.append(s+"\n");
+	static class Person {
+		String name;
+		int cntChilds;
+		ArrayList<Person> parents;
+		ArrayList<String> childs;
+		public Person(String name) {
+			this.name = name;
+			this.parents = new ArrayList<>();
+			this.childs = new ArrayList<>();
 		}
-		System.out.println(sb);
-	}
-	static void search() {
-		while(!primes.isEmpty()){
-			int parent = primes.poll();
-			String childs = "";
-			int cntChild = 0;
-			for(int child:map.get(parent)) {
-				depth[child] --;
-				if(depth[child]==0) { //이때 parent가 child의 부모
-					if(cntChild == 0) childs += peoples[child];
-					else childs += " "+peoples[child];
-					cntChild++;
-					primes.add(child);
-				}
-			}
-			result.add(peoples[parent]+" "+cntChild+" "+childs);
+		public void appendParent(Person parent) {
+			this.parents.add(parent);
+		}
+		@Override
+		public String toString() {
+			return "Person [name=" + name + ", parents=" + parents + "]";
 		}
 	}
-	//대빵 조상 찾기
-	static void getPrimes() {
-		String prime = "";
-		int cntPrime = 0;
-		for(int i=0;i<N;i++) {
-			if(depth[i]==0) {
-				primes.add(i);
-				if(cntPrime==0) prime += peoples[i];
-				else prime += " "+peoples[i];
-				cntPrime++;
-			}
-		}
-		sb.append(cntPrime+"\n");
-		sb.append(prime+"\n");
-	}
-	static void init() throws Exception {
+	static int N, M;
+	static HashMap<Integer, Person> persons;
+	static HashMap<String, Integer> indexing;
+	static ArrayList<Person> list;
+	static int idx;
+	static int cnt;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
 		N = Integer.parseInt(br.readLine());
-		peoples = new String[N];
-		depth = new int[N];
-		String[] inputPeoples = br.readLine().split(" ");
-		for(int i=0;i<N;i++) {
-			peoples[i] = inputPeoples[i];
+		persons = new HashMap<>();
+		indexing = new HashMap<>();
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			String name = st.nextToken();
+			Person tmp = new Person(name);
+			indexing.put(name, idx);
+			persons.put(idx++, tmp);
 		}
-		//사람들 알파벳순으로 오름차순해서 인덱스 매기기
-		Arrays.sort(peoples);
-		for(int i=0;i<N;i++) {
-			nameMap.put(peoples[i], i);
-			map.put(i, new ArrayList<>());
-		}
-		//매핑 정보 
+		
 		M = Integer.parseInt(br.readLine());
-		for(int i=0;i<M;i++) {
-			String[] input = br.readLine().split(" ");
-			int child = nameMap.get(input[0]);
-			int parent = nameMap.get(input[1]);
-			depth[child]++; //자식 레벨 증가
-			map.get(parent).add(child);
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			String child = st.nextToken();
+			String parent = st.nextToken();
+			int index = indexing.get(child);
+			
+			persons.get(index).appendParent(persons.get(indexing.get(parent)));
 		}
-		// 자식들도 미리 오름차순 정렬한다.
-		for(int i=0;i<N;i++) {
-			Collections.sort(map.get(i));
+		
+//		persons.forEach((key, value) -> {
+//			System.out.println("index : " + key);
+//			System.out.println("name : " + value.name);
+//			System.out.println("parents : " + value.parents.toString());
+//			System.out.println();
+//		});
+		
+		list = new ArrayList<>();
+		
+		persons.forEach((key, value) -> {
+			list.add(value);
+		});
+		
+		Collections.sort(list, new Comparator<Person>() {
+			@Override
+			public int compare(Person o1, Person o2) {
+				return o1.name.compareTo(o2.name);
+			}
+		});
+		
+		for (Person p : list) {
+			if (p.parents.size() > 1) {
+				Collections.sort(p.parents, new Comparator<Person>() {
+					@Override
+					public int compare(Person o1, Person o2) {
+						return o2.parents.size() - o1.parents.size();
+					}
+				});
+				p.parents.get(0).cntChilds++;
+				p.parents.get(0).childs.add(p.name);
+			}
+			if (p.parents.size() == 1) {
+				p.parents.get(0).cntChilds++;
+				p.parents.get(0).childs.add(p.name);
+			}
 		}
+		
+		for (Person p : list) {
+			if (p.parents.size() == 0)
+				cnt++;
+		}
+		System.out.println(cnt);
+		for (Person p : list) {
+			if (p.parents.size() == 0)
+				System.out.print(p.name + " ");
+		}
+		System.out.println();
+		
+		for (Person p : list) {
+			System.out.print(p.name + " ");
+			System.out.print(p.cntChilds + " ");
+			for (int i = 0; i < p.childs.size(); i++) {
+				System.out.print(p.childs.get(i) + " ");
+			}
+			System.out.println();
+		}
+		
 	}
+
 }
