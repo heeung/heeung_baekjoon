66fd0d3c9ed302be05f53ccd2a3d57604265d2ad
heeung <94839275+heeung@users.noreply.github.com>
2023-03-22 20:45:42 +0900

    [Gold IV] Title: 플로이드, Time: 632 ms, Memory: 44112 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,67 +1,67 @@
-
-import java.util.*;
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N,M;
-	static int[][] distance;
-	static StringBuffer sb = new StringBuffer();
-	
-	public static void main(String[] args) throws Exception{
-		init();
-		
-		
-		floydWarshall();
-		
-		
-		for(int i=0;i<N;i++) {
-			String s = "";
-			for(int j=0;j<N;j++) {
-				if(distance[i][j]==10_000_000) distance[i][j] = 0;
-				if(j==0) s += distance[i][j]+"";
-				else s += " "+distance[i][j]; 
-			}
-			sb.append(s+"\n");
+	static int[][] graph, res;
+	static int N, M;
+	static boolean[] visited;
+	static int cnt, min;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+
+		graph = new int[N][N];
+		res = new int[N][N];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken()) - 1;
+			int to = Integer.parseInt(st.nextToken()) - 1;
+			int num = Integer.parseInt(st.nextToken());
+			if (graph[from][to] != 0)
+				graph[from][to] = Math.min(graph[from][to], num);
+			else
+				graph[from][to] = num;
 		}
 		
-		System.out.println(sb);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (graph[i][j] == 0)
+					graph[i][j] = 300000000;
+			}
+		}
+		pickBest();
 		
-	}
-	
-	
-	static void floydWarshall() {
-		for(int k=0;k<N;k++) {
-			for(int i=0;i<N;i++) {
-				for(int j=0;j<N;j++) {
-					distance[i][j] = Math.min(distance[i][j], distance[i][k]+distance[k][j]);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (i == j) {
+					graph[i][j] = 0;
+				}
+                if (graph[i][j] == 300000000) {
+					graph[i][j] = 0;
 				}
 			}
 		}
-	}
-	
-	static void init() throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
 		
-		distance = new int[N][N];
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				if(i==j) distance[i][j] = 0;
-				else distance[i][j] = 10_000_000;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(graph[i][j] + " ");
 			}
+			System.out.println();
 		}
-		
-		for(int i=0;i<M;i++) {
-			String[] input = br.readLine().split(" ");
-			int node1 = Integer.parseInt(input[0])-1;
-			int node2 = Integer.parseInt(input[1])-1;
-			int cost = Integer.parseInt(input[2]);
-			
-			distance[node1][node2] = Math.min(distance[node1][node2], cost);
-		}
-		
+	}
+
+	public static void pickBest() {
+		for (int mid = 0; mid < N; mid++)
+			for (int from = 0; from < N; from++)
+				for (int to = 0; to < N; to++)
+					graph[from][to] = Math.min(graph[from][mid] + graph[mid][to], graph[from][to]);
 	}
 }
