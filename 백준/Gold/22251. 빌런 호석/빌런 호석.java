b9c7fec91e7108fb27952cfe7caaa7043c7c519d
heeung <94839275+heeung@users.noreply.github.com>
2023-03-25 14:05:59 +0900

    [Gold V] Title: 빌런 호석, Time: 156 ms, Memory: 14932 KB -BaekjoonHub

--- 백준/Gold/22251. 빌런 호석/빌런 호석.java
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.java
@@ -1,67 +1,136 @@
-
-import java.util.*;
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int N, K, P, X, cnt, ret, check;
+	static int[][] cost = new int[10][10];
+	static int[][] nums = { { 1, 1, 1, 0, 1, 1, 1 }, { 0, 0, 1, 0, 0, 1, 0 }, { 1, 0, 1, 1, 1, 0, 1 },
+			{ 1, 0, 1, 1, 0, 1, 1 }, { 0, 1, 1, 1, 0, 1, 0 }, { 1, 1, 0, 1, 0, 1, 1 }, { 1, 1, 0, 1, 1, 1, 1 },
+			{ 1, 0, 1, 0, 0, 1, 0 }, { 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 0, 1, 1 } };
+	static int[] tmaxFloor, nowFloor, maxFloor;
+	static int curFloor;
 
-	static int[][] changeCnt = {
-			{0,4,3,3,4,3,2,3,1,2},
-			{4,0,5,3,2,5,6,1,5,4},
-			{3,5,0,2,5,4,3,4,2,3},
-			{3,3,2,0,3,2,3,2,2,1},
-			{4,2,5,3,0,3,4,3,3,2},
-			{3,5,4,2,3,0,1,4,2,1},
-			{2,6,3,3,4,1,0,5,1,2},
-			{3,1,4,2,3,4,5,0,4,3},
-			{1,5,2,2,3,2,1,4,0,1},
-			{2,4,3,1,2,1,2,3,1,0}
-	};
-	static int N,K,P,X;
-	static String S="";
-	static HashSet<String> set = new HashSet<>();
-	static List<int[]> combs = new ArrayList<>();
-	static int answer = 0;
-	
-	public static void main(String[] args) throws Exception{
-		init();
-		
-		dfs(0,"",0);
-		
-		System.out.println(answer);
-		
-	}
-	
-	static void dfs(int idx, String s, int cnt) {
-		if(idx==K) {
-			int result = Integer.parseInt(s);
-			if(1<=result && result<=N && result!=X) {
-//				System.out.println(s);
-				answer++;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+
+		// N : 층수
+		N = Integer.parseInt(st.nextToken());
+		// K : 최대 자리 수
+		K = Integer.parseInt(st.nextToken());
+		// P : 최대 반전 개수
+		P = Integer.parseInt(st.nextToken());
+		// X : 지금 멈춰 있는 층
+		X = Integer.parseInt(st.nextToken());
+
+		makeCostArr();
+//		System.out.println("    0, 1, 2, 3, 4, 5, 6, 7, 8, 9");
+//		System.out.println();
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println(i + "  " + Arrays.toString(cost[i]));
+//		}
+
+		int tmpN = N;
+		tmaxFloor = new int[K];
+		for (int i = K - 1; i >= 0; i--) {
+			tmaxFloor[i] = tmpN % 10;
+			tmpN = tmpN / 10;
+			if (tmpN == 0) {
+				K = K - i;
+				break;
 			}
+		}
+
+		maxFloor = new int[K];
+		for (int i = K - 1; i >= 0; i--) {
+			maxFloor[i] = tmaxFloor[i + tmaxFloor.length - K];
+		}
+
+		nowFloor = new int[K];
+		for (int i = K - 1; i >= 0; i--) {
+			nowFloor[i] = X % 10;
+			X = X / 10;
+		}
+		solve(0);
+		System.out.println(ret - 1);
+	}
+
+	public static void makeCostArr() {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (i == j)
+					continue;
+				cost[i][j] = getDiff(i, j);
+			}
+		}
+	}
+
+	public static int getDiff(int from, int to) {
+		int cnt = 0;
+		for (int i = 0; i < 7; i++) {
+			if (nums[from][i] != nums[to][i]) {
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+
+	public static void solve(int depth) {
+
+		if (depth == K) {
+			if (check == 0 || curFloor > N)
+				return;
+			if (cnt <= P)
+				ret++;
 			return;
 		}
-		int num = S.charAt(idx)-'0';
-		for(int i=0;i<10;i++) {
-			if(cnt+changeCnt[num][i]<=P) {
-				dfs(idx+1,s+i,cnt+changeCnt[num][i]);
+
+		if (cnt > P)
+			return;
+
+		if (depth == 0) {
+			for (int i = 0; i <= maxFloor[0]; i++) {
+				cnt += cost[nowFloor[depth]][i];
+				check += i;
+				curFloor = curFloor * 10 + i;
+				solve(depth + 1);
+				curFloor = curFloor / 10;
+				cnt -= cost[nowFloor[depth]][i];
+			}
+		} else {
+			for (int i = 0; i < 10; i++) {
+				cnt += cost[nowFloor[depth]][i];
+				check += i;
+				curFloor = curFloor * 10 + i;
+				solve(depth + 1);
+				curFloor = curFloor / 10;
+				cnt -= cost[nowFloor[depth]][i];
 			}
 		}
-		
 	}
-	
-	
-	static void init() throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] NKPX = br.readLine().split(" ");
-		N = Integer.parseInt(NKPX[0]);
-		K = Integer.parseInt(NKPX[1]);
-		P = Integer.parseInt(NKPX[2]);
-		X = Integer.parseInt(NKPX[3]);
-		String SX = String.valueOf(X);
-		for(int i=0;i<K;i++) {
-			if(i>=K-SX.length()) S += SX.charAt(i-(K-SX.length()));
-			else S += "0";
+
+	public static void solve2(int depth) {
+		
+		if (cnt > P)
+			return ;
+
+		if (depth == K) {
+			ret++;
+			return;
+		}
+		
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(maxFloor[0] + " " + depth + " " + i);
+			cnt += cost[nowFloor[depth]][i];
+			solve2(depth + 1);
+			cnt -= cost[nowFloor[depth]][i];
 		}
 	}
+
 }
