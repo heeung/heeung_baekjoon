0793511b0747e790d8f1ef62cf48426378d84721
heeung <94839275+heeung@users.noreply.github.com>
2023-03-29 11:45:36 +0900

    [Gold II] Title: 꿈틀꿈틀 호석 애벌레 - 효율성, Time: 348 ms, Memory: 26776 KB -BaekjoonHub

--- 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.java
+++ 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.java
@@ -1,85 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static class Eat implements Comparable<Eat>{
-		int size;
-		int cnt;
-		int idx;
-		public Eat(int size, int cnt, int idx) {
-			this.size = size;
-			this.cnt = cnt;
-			this.idx = idx;
-		}
-		@Override
-		public int compareTo(Eat o) {
-			return o.size - this.size;
-		}
-	}	
-	static int N, K;;
-	static boolean[] check;
-	static int[][] arr;
-	static PriorityQueue<Eat> eats = new PriorityQueue<>();
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		arr = new int[2][N];
-		
-		st = new StringTokenizer(br.readLine());
 	
-		int sum = 0;
-		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-			int n = Integer.parseInt(st.nextToken());
-			
-			sum += n;
-			if (sum < K) {
-				arr[0][i] = sum;
-				arr[1][i] = ++cnt;
-			} else {
-				arr[0][i] = sum;
-				arr[1][i] = ++cnt;
-				cnt = 1;
-				sum = n;
-			}
-			eats.offer(new Eat(arr[0][i], arr[1][i], i));
-		}
-		
-//		System.out.println(Arrays.toString(arr[0]));
-//		System.out.println(Arrays.toString(arr[1]));
-		
-		check = new boolean[N];
-		cnt = 0;
-		sum = 0;
-		while (!eats.isEmpty()) {
-			Eat cur = eats.poll();
-			
-			if (!check[cur.idx] && !check[cur.idx - (cur.cnt - 1)] && cur.size >= K) {
-				cnt++;
-				sum += cur.size;
-				
-				int n = cur.cnt;
-				while (n-- > 0) {
-					check[cur.idx--] = true;
-				}
-			}
-		}
-		
-//		System.out.println(Arrays.toString(check));
-//		System.out.println("sum : " + sum);
-//		System.out.println("cnt : " + cnt);
-		System.out.println(sum - (cnt * K));
+	static int N, K;
+	static int[] arr;
+	static long[] dp;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	}
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        dp = new long[N + 1];
+        long sum = arr[0];
+        int leftIdx = 0;
+        int rightIdx = 1;
+
+        while (rightIdx <= N) {
+            if (sum >= K) {
+                while (sum >= K) {
+                    dp[rightIdx] = Math.max(dp[rightIdx], dp[leftIdx] + sum - K);
+                    sum -= arr[leftIdx++];
+                }
+            } else {
+                dp[rightIdx] = Math.max(dp[rightIdx], dp[rightIdx - 1]);
+
+                if (rightIdx == N)
+                    break;
+
+                sum += arr[rightIdx++];
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
 
 }
