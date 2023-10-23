b3f2dc25b3281bdeb94cce5f814fdd2263719be0
heeung <94839275+heeung@users.noreply.github.com>
2023-10-23 20:12:56 +0900

    [Gold IV] Title: 카드 섞기, Time: 204 ms, Memory: 63288 KB -BaekjoonHub

---
+++ 백준/Gold/1091. 카드 섞기/카드 섞기.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, cnt;
+    static int[] S, P, cards, preCards;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        P = new int[N];
+        S = new int[N];
+        cards = new int[N];
+        preCards = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            S[i] = Integer.parseInt(st.nextToken());
+            cards[i] = i;
+            preCards[i] = i;
+        }
+
+        while (!check()) {
+            // 섞고있음에도 불구하고 처음과 같은게 나와버리면 계속 순환된다는 의미임
+            if (cnt != 0 && Arrays.equals(cards, preCards)) {
+                System.out.println("-1");
+                return ;
+            }
+            cnt += 1;
+            shuffle();
+        }
+
+        System.out.println(cnt);
+    }
+
+    static void shuffle() {
+        int[] t = new int[N];
+        for (int i =0; i < N; i++) {
+            t[S[i]] = cards[i];
+        }
+        cards = t.clone();
+    }
+
+    static boolean check() {
+        for (int i = 0; i < N; i++) {
+            int x = cards[i];
+            if (P[x] != i % 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
